fc89bfce7af1b2267afceb750f0c3cf5ab013193
bvvy <bvvy@qq.com>
2019-10-17 23:29:32 +0800

    简单的组件 明天再进一步整合

---
+++ src/components/temp/TTitle.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="t-title">
+        <span>{{title}}</span>
+        <span class="t-sub">{{sub}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TTitle',
+        props: {
+            title: {
+                type: String,
+            },
+            sub: {
+               type: String,
+               default: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .t-sub {
+        font-size: 12px;
+        color: #666;
+    }
+
+    .t-title {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
